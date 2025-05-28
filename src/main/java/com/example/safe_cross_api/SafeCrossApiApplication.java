06e883cfdfa2ba4c151653549f744c305e10f9f6
MatheusCosta616 <matheus.costa2616@gmail.com>
2025-05-28 16:17:07 -0300

    Base do projeto

---
+++ src/main/java/com/example/safe_cross_api/SafeCrossApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.safe_cross_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeCrossApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeCrossApiApplication.class, args);
+	}
+
+}
